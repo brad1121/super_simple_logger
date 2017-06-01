68ba1efabb9f1c53527890d48ad451daa3411e4f
Brad <brad1121@gmail.com>
2017-06-01 14:22:40 +1000

    herp derp

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const fs = require('fs');
 var os = require("os");
 module.exports = (level,message,log_path = './super_simple_logger.txt')=>{
 	return new Promise((resolve,reject)=>{
-		if(typeof message == 'Array' || typeof message == 'Object' ){
+		console.log(typeof message);
+		if(typeof message == 'array' || typeof message == 'object' ){
 			message = JSON.stringify(message);
 		}
-	if(fs.existsSync(log_path)){
+	if(fs.existsSync(log_path) || log_path == './super_simple_logger.txt'){
 	let log_line = new Date().toISOString().replace("T"," ")+" ["+level+"] "+message+os.EOL;
 	fs.appendFile(log_path,log_line,'utf8',(err)=>{
 		 if (err) reject(err);
